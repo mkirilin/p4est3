e787e50ced9cf0b3132135aa46c8421c3187ce49
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2019-12-10 16:17:23 +0100

    Make stubs for 3D p4est3 files

---
+++ src/p8est3.h
@@ -0,0 +1,52 @@
+/*
+  This file is part of p4est, version 3
+  p4est is a C library to manage a collection (a forest) of multiple
+  connected adaptive quadtrees or octrees in parallel.
+
+  Copyright (C) 2019 individual authors
+  Originally written by Carsten Burstedde, Lucas C. Wilcox, and Tobin Isaac
+
+  p4est is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  p4est is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with p4est; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+#ifndef P8EST3_H
+#define P8EST3_H
+
+#if 0 /* 3D not ready yet */
+
+#include <sc.h>
+#include <p4est3_base.h>
+#include <p8est3_quadrant_vtable.h>
+
+#ifdef __cplusplus
+extern              "C"
+{
+#if 0
+}
+#endif
+#endif
+
+/** In the 3D implementation, this is the number of a quadrant's children. */
+#define P8EST3_CHILDREN 8
+
+#endif /* 0 */
+
+#ifdef __cplusplus
+#if 0
+{
+#endif
+}
+#endif
+
