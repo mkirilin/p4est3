1b35fe462d9ff85a53efdae218edb75683305f6a
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2020-02-19 18:35:10 +0100

    Begin p4est3 connectivity object

---
+++ src/p4est3_connectivity.c
@@ -0,0 +1,166 @@
+/*
+  This file is part of p4est, version 3.
+  p4est is a C library to manage a collection (a forest) of multiple
+  connected adaptive quadtrees or octrees in parallel.
+
+  Copyright (C) 2019 individual authors
+  Originally written by Carsten Burstedde, Lucas C. Wilcox, and Tobin Isaac
+
+  p4est is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  p4est is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with p4est; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+#include <p4est3_connectivity.h>
+#include <sc3_refcount_internal.h>
+
+struct p4est3_connectivity
+{
+  sc3_refcount_t      rc;
+  sc3_allocator_t    *alloc;
+  int                 setup;
+
+  /* parameters fixed after setup call */
+  p4est3_topidx       num_trees;
+};
+
+int
+p4est3_connectivity_is_valid (const p4est3_connectivity_t * c, char *reason)
+{
+  SC3E_TEST (c != NULL, reason);
+  SC3E_IS (sc3_refcount_is_valid, &c->rc, reason);
+  SC3E_IS (sc3_allocator_is_setup, c->alloc, reason);
+  SC3E_TEST (c->num_trees > 0, reason);
+  SC3E_YES (reason);
+}
+
+int
+p4est3_connectivity_is_new (const p4est3_connectivity_t * c, char *reason)
+{
+  SC3E_IS (p4est3_connectivity_is_valid, c, reason);
+  SC3E_TEST (!c->setup, reason);
+  SC3E_YES (reason);
+}
+
+int
+p4est3_connectivity_is_setup (const p4est3_connectivity_t * c, char *reason)
+{
+  SC3E_IS (p4est3_connectivity_is_valid, c, reason);
+  SC3E_TEST (c->setup, reason);
+  SC3E_YES (reason);
+}
+
+sc3_error_t        *
+p4est3_connectivity_new (sc3_allocator_t * alloc, p4est3_connectivity_t ** pc)
+{
+  p4est3_connectivity_t *c;
+
+  SC3E_RETVAL (pc, NULL);
+  SC3A_IS (sc3_allocator_is_setup, alloc);
+
+  SC3E (sc3_allocator_ref (alloc));
+  SC3E_ALLOCATOR_CALLOC (alloc, p4est3_connectivity_t, 1, c);
+  SC3E (sc3_refcount_init (&c->rc));
+  c->alloc = alloc;
+  c->num_trees = 1;
+  SC3A_IS (p4est3_connectivity_is_new, c);
+
+  *pc = c;
+  return NULL;
+}
+
+sc3_error_t        *
+p4est3_connectivity_set_num_trees (p4est3_connectivity_t * c,
+                                   p4est3_topidx num_trees)
+{
+  SC3A_IS (p4est3_connectivity_is_new, c);
+  SC3A_CHECK (num_trees > 0);
+  c->num_trees = num_trees;
+  return NULL;
+}
+
+sc3_error_t        *
+p4est3_connectivity_setup (p4est3_connectivity_t * c)
+{
+  SC3A_IS (p4est3_connectivity_is_new, c);
+  c->setup = 1;
+  SC3A_IS (p4est3_connectivity_is_setup, c);
+  return NULL;
+}
+
+sc3_error_t        *
+p4est3_connectivity_ref (p4est3_connectivity_t * c)
+{
+  SC3A_IS (p4est3_connectivity_is_setup, c);
+  SC3E (sc3_refcount_ref (&c->rc));
+  return NULL;
+}
+
+sc3_error_t        *
+p4est3_connectivity_unref (p4est3_connectivity_t ** pc)
+{
+  int                 waslast;
+  sc3_allocator_t    *alloc;
+  p4est3_connectivity_t *c;
+
+  SC3E_INOUTP (pc, c);
+  SC3A_IS (p4est3_connectivity_is_valid, c);
+
+  SC3E (sc3_refcount_unref (&c->rc, &waslast));
+  if (waslast) {
+    *pc = NULL;
+
+    alloc = c->alloc;
+    SC3E_ALLOCATOR_FREE (alloc, p4est3_connectivity_t, c);
+    SC3E (sc3_allocator_unref (&alloc));
+  }
+  return NULL;
+}
+
+sc3_error_t        *
+p4est3_connectivity_destroy (p4est3_connectivity_t ** pc)
+{
+  p4est3_connectivity_t *c;
+
+  SC3E_INULLP (pc, c);
+  SC3E_DEMIS (sc3_refcount_is_last, &c->rc);
+  SC3E (p4est3_connectivity_unref (&c));
+
+  SC3A_CHECK (c == NULL);
+  return NULL;
+}
+
+sc3_error_t        *
+p4est3_connectivity_get_num_trees (const p4est3_connectivity_t * c,
+                                   p4est3_topidx * pnum_trees)
+{
+  SC3A_IS (p4est3_connectivity_is_setup, c);
+  SC3A_CHECK (pnum_trees != NULL);
+  *pnum_trees = c->num_trees;
+  return NULL;
+}
+
+sc3_error_t        *
+p4est3_connectivity_new_unitcube (sc3_allocator_t * alloc,
+                                  p4est3_connectivity_t ** pc)
+{
+  p4est3_connectivity_t *c;
+
+  SC3E_RETVAL (pc, NULL);
+  SC3E (p4est3_connectivity_new (alloc, &c));
+  SC3E (p4est3_connectivity_setup (c));
+  SC3A_IS (p4est3_connectivity_is_setup, c);
+
+  *pc = c;
+  return NULL;
+}
---
+++ src/p4est3_connectivity.h
@@ -0,0 +1,71 @@
+/*
+  This file is part of p4est, version 3.
+  p4est is a C library to manage a collection (a forest) of multiple
+  connected adaptive quadtrees or octrees in parallel.
+
+  Copyright (C) 2019 individual authors
+  Originally written by Carsten Burstedde, Lucas C. Wilcox, and Tobin Isaac
+
+  p4est is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  p4est is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with p4est; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+#ifndef P4EST3_CONNECTIVITY_H
+#define P4EST3_CONNECTIVITY_H
+
+#include <p4est3_base.h>
+
+#ifdef __cplusplus
+extern              "C"
+{
+#if 0
+}
+#endif
+#endif
+
+typedef struct p4est3_connectivity p4est3_connectivity_t;
+
+int                 p4est3_connectivity_is_valid (const p4est3_connectivity_t
+                                                  * c, char *reason);
+int                 p4est3_connectivity_is_new (const p4est3_connectivity_t *
+                                                c, char *reason);
+int                 p4est3_connectivity_is_setup (const p4est3_connectivity_t
+                                                  * c, char *reason);
+
+sc3_error_t        *p4est3_connectivity_new (sc3_allocator_t * alloc,
+                                             p4est3_connectivity_t ** pc);
+
+sc3_error_t        *p4est3_connectivity_set_num_trees
+  (p4est3_connectivity_t * c, p4est3_topidx num_trees);
+
+sc3_error_t        *p4est3_connectivity_setup (p4est3_connectivity_t * c);
+sc3_error_t        *p4est3_connectivity_ref (p4est3_connectivity_t * c);
+sc3_error_t        *p4est3_connectivity_unref (p4est3_connectivity_t ** c);
+sc3_error_t        *p4est3_connectivity_destroy (p4est3_connectivity_t ** c);
+
+sc3_error_t        *p4est3_connectivity_get_num_trees
+  (const p4est3_connectivity_t * c, p4est3_topidx * pnum_trees);
+
+sc3_error_t        *p4est3_connectivity_new_unitcube (sc3_allocator_t * alloc,
+                                                      p4est3_connectivity_t **
+                                                      pc);
+
+#ifdef __cplusplus
+#if 0
+{
+#endif
+}
+#endif
+
+#endif /* !P4EST3_CONNECTIVITY_H */
