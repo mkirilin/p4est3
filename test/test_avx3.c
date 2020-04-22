4e6a76173b881c72de310affe2734e0ae5503ccc
Mikhail Kirilin <kirilin@ins.uni-bonn.de>
2020-04-22 13:02:55 +0200

    Avx: update copyright

--- test/test_avx3.c
+++ test/test_avx3.c
@@ -1,11 +1,10 @@
 /*
-  This file is part of p4est.
+  This file is part of p4est, version 3.
   p4est is a C library to manage a collection (a forest) of multiple
   connected adaptive quadtrees or octrees in parallel.
 
-  Copyright (C) 2010 The University of Texas System
-  Additional copyright (C) 2011 individual authors
-  Written by Carsten Burstedde, Lucas C. Wilcox, and Tobin Isaac
+  Copyright (C) 2019 individual authors
+  Originally written by Carsten Burstedde, Lucas C. Wilcox, and Tobin Isaac
 
   p4est is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
