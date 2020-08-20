fcaddce7b6f354176ce1134f226065b46456b2a8
Mikhail Kirilin <kirilin@ins.uni-bonn.de>
2020-08-20 14:24:24 +0200

    Qvt: add common ancestor, linear id, is ancestor

--- src/p4est3_quadrant_vtable.h
+++ src/p4est3_quadrant_vtable.h
@@ -49,6 +49,13 @@ typedef sc3_error_t *(*p4est3_quadrant_in_i_out_t) (const void *q, int i,
                                                     void *r);
 typedef sc3_error_t *(*p4est3_quadrant_morton_t) (int l, p4est3_gloidx i,
                                                   void *r);
+typedef sc3_error_t *(*p4est3_nearest_common_ancestor_t) (const void *q1,
+                                                          const void *q2,
+                                                          void *r);
+typedef sc3_error_t *(*p4est3_quadrant_linear_id_t) (const void *q, int l,
+                                                     p4est3_gloidx *i);
+typedef sc3_error_t *(*p4est3_quadrant_is_ancestor_t) (const void *q1,
+                                                       const void *q2, int *j);
 
 typedef p4est3_quadrant_in_j_t p4est3_quadrant_level_t;
 typedef p4est3_quadrant_in_j_t p4est3_quadrant_child_id_t;
@@ -97,6 +104,9 @@ typedef struct p4est3_quadrant_vtable
   p4est3_quadrant_first_descendant_t quadrant_first_descendant;
   p4est3_quadrant_last_descendant_t quadrant_last_descendant;
   p4est3_quadrant_morton_t quadrant_morton;
+  p4est3_nearest_common_ancestor_t nearest_common_ancestor;
+  p4est3_quadrant_linear_id_t quadrant_linear_id;
+  p4est3_quadrant_is_ancestor_t quadrant_is_ancestor;
 }
 p4est3_quadrant_vtable_t;
 
@@ -142,8 +152,18 @@ sc3_error_t        *p4est3_quadrant_last_descendant (p4est3_quadrant_vtable_t
                                                      int l, void *r);
 /* This function works up to level l=21 */
 sc3_error_t        *p4est3_quadrant_morton (p4est3_quadrant_vtable_t * qvt,
-                                            int l, p4est3_gloidx id,
-                                            void *r);
+                                            int l, p4est3_gloidx id, void *r);
+sc3_error_t        *p4est3_nearest_common_ancestor (p4est3_quadrant_vtable_t
+                                                    * qvt, const void *q1,
+                                                    const void *q2, void *r);
+
+sc3_error_t        *p4est3_quadrant_linear_id (p4est3_quadrant_vtable_t * qvt,
+                                               const void *q, int l,
+                                               p4est3_gloidx * id);
+
+sc3_error_t        *p4est3_quadrant_is_ancestor (p4est3_quadrant_vtable_t
+                                                 * qvt, const void *q1,
+                                                 const void *q2, int *j);
 
 /**************************** static functions *****************************/
 
