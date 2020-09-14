62eae6fe17bbb6284d6d87df10fcbfda9c6603f9
Mikhail Kirilin <kirilin@ins.uni-bonn.de>
2020-09-14 16:53:34 +0200

    Fix: typecasting while computing quadbytes

--- src/p4est3_internal_mpi.c
+++ src/p4est3_internal_mpi.c
@@ -278,7 +278,7 @@ p4est3_internal_setup_tree (p4est3_t * p3, p4est3_gloidx num_uniform)
   /* create shared quadrant storage */
   SC3E (sc3_allocator_malloc (p3->alloc, p3->nodesize * sizeof (char *),
                               &p3->nodequads));
-  quadbytes = p3->local_num_quads * p3->qsize;
+  quadbytes = (sc3_MPI_Aint_t) p3->local_num_quads * p3->qsize;
   SC3E (sc3_MPI_Win_allocate_shared
         (quadbytes, p3->qsize,
          p3->info_noncontig, p3->nodecomm, &quadmem, &p3->quadwin));
