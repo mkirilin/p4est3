140902b4806932cff8e7ae3ae8c0234c2db9cde2
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2020-12-01 18:16:14 +0100

    Begin documenting p4est3_internal.h

--- src/p4est3_internal.h
+++ src/p4est3_internal.h
@@ -21,6 +21,17 @@
   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 */
 
+/** \file p4est3_internal.h
+ *
+ * Private declarations for use within the library.
+ * It must never be included by public header files.
+ *
+ * Me make no provisions on stability or backwards compatibility.
+ * There is usually no reason to use them outside of the library.
+ *
+ * \ingroup p4est3
+ */
+
 #ifndef P4EST3_INTERNAL_H
 #define P4EST3_INTERNAL_H
 
@@ -28,64 +39,82 @@
 #include <sc3_refcount.h>
 #include <p4est3.h>
 
+/** Data structure for a process-local tree and the quadrants it contains. */
 typedef struct p4est3_tree
 {
-  p4est3_topidx       treeid;
-  p4est3_gloidx       first_tquad, end_tquad;
-  p4est3_locidx       quad_offset;
-  p4est3_locidx       num_quads;
-  char               *tquads;
+  p4est3_topidx       treeid;   /**< Tree number is zero based. */
+  p4est3_gloidx       first_tquad;
+  p4est3_gloidx       end_tquad;
+  p4est3_locidx       quad_offset;      /**< Local quadrants before this tree. */
+  p4est3_locidx       num_quads;        /**< Local quadrants within this tree. */
+  char               *tquads;   /**< Array of local tree quadrants. */
 }
 p4est3_tree_t;
 
+/** This internal structure holds the members of a forest object.
+ * Don't rely on its declaration in code outside the library. */
 struct p4est3
 {
-  sc3_refcount_t      rc;
-  sc3_allocator_t    *alloc;
-  int                 setup;
+  /* variables of internal state used during the whole lifetime */
+  sc3_refcount_t      rc;       /**< Reference counter in use. */
+  sc3_allocator_t    *alloc;    /**< Memory allocator in use. */
+  int                 setup;    /**< Boolean: object is setup. */
+  int                 accessed_conn;    /**< Number of currently active
+                                             connectivity accesses. */
 
   /* this forest may be wrapping a virtual implementation */
-  p4est3_vtable_t     spvt, *pvt;
-  void               *slf;
+  p4est3_vtable_t     spvt;     /**< Memory pointed to by \ref pvt. */
+  p4est3_vtable_t     *pvt;     /**< If not NULL, forest virtual table. */
+  void               *slf;      /**< Context to use with virtual forest. */
 
-  sc3_MPI_Comm_t      mpicomm;
-  int                 commdup;
-  p4est3_connectivity_t *conn;
+  /* variables set before \ref p4est3_setup */
+  sc3_MPI_Comm_t      mpicomm;  /**< Valid MPI communicator. */
+  int                 commdup;  /**< Boolean: communicator has been duped. */
+  p4est3_connectivity_t *conn;  /**< Pointer to the relevant connectivity. */
+  p4est3_quadrant_vtable_t sqvt;        /**< Memory pointed to by \ref qvt.
+                                             Stores virtual quadrant methods. */
+  p4est3_quadrant_vtable_t *qvt;        /**< Always points to \ref sqvt. */
+  p4est3_topidx       num_trees;        /**< Convenience variable to store
+                                             the number of trees in \c conn. */
+  int                 level;    /**< Configuration variable for initiel level. */
 
-  int                 accessed_conn;
-
-  sc3_MPI_Comm_t      nodecomm, headcomm;
+  /* variables populated during \ref p4est3_setup */
+  sc3_MPI_Comm_t      nodecomm;
+  sc3_MPI_Comm_t      headcomm;
   sc3_MPI_Info_t      info_noncontig;
   sc3_MPI_Win_t       nodesizewin;
   sc3_MPI_Win_t       gfposwin, gftreewin, goffsetwin;
   sc3_MPI_Win_t       quadwin;
-  int                 mpisize, mpirank;
-  int                 nodesize, noderank;
+  int                 mpisize;          /**< Size of forest communicator. */
+  int                 mpirank;          /**< Rank in forest communicator. */
+  int                 nodesize;
+  int                 noderank;
   int                 num_nodes;
   int                 node_num;
   int                 node_frank;
   int                *node_sizes;
   int                *node_offsets;
 
-  p4est3_quadrant_vtable_t sqvt, *qvt;
-  p4est3_topidx       num_trees;
-  int                 level;
   int                 qmaxlevel;
   int                 num_children;
-  int                 qsize;
+  int                 qsize;            /**< Store byte size of one quadrant. */
 
-  int                 max_threads;
+  int                 max_threads;      /**< Max threads from querying openmp. */
   char              **temp_quad;
 
-  p4est3_locidx       local_num_quads;
-  p4est3_gloidx       global_num_quads;
+  p4est3_locidx       local_num_quads;  /**< Count process-local quadrants. */
+  p4est3_gloidx       global_num_quads; /**< Count all quadrants globally. */
   p4est3_gloidx      *goffset;
   p4est3_topidx      *gftree;
   char               *gfpos;
-  char              **nodequads, *quads;
+  char              **nodequads;
+  char               *quads;
 
-  p4est3_topidx       fltree, lltree, nltrees;
-  sc3_array_t        *trees;
+  p4est3_topidx       fltree;   /**< Number of first local tree.
+                                     Relative to all trees in \ref conn. */
+  p4est3_topidx       lltree;   /**< Number of last local tree inclusive. */
+  p4est3_topidx       nltrees;  /**< Number of process-local trees. */
+  sc3_array_t        *trees;    /**< Array of only the process-local trees. */
 };
 
 #ifdef __cplusplus
@@ -96,9 +125,20 @@ extern              "C"
 #endif
 #endif
 
+/** Index into array of local trees and return pointer to indexed tree.
+ * \param [in] p3   Required to be non-NULL.
+ *                  For speed, we do not check for validity each time.
+ * \param [in] tt   Must be the index of a local tree, thus between \ref
+ *                  p4est3_t::fltree and \ref p4est3_t::lltree inclusive.
+ * \param [in,out] tree     Non-NULL on input.  On output, value is
+ *                          populated with a pointer to local tree structure.
+ * \return                  NULL on success, error object otherwise.
+ */
 sc3_error_t        *p4est3_tree_index (p4est3_t * p3, p4est3_topidx tt,
                                        p4est3_tree_t ** tree);
 
+/** \cond P4EST_FALSE */
+/* these functions are not documented on purpose */
 sc3_error_t        *p4est3_internal_setup_comm (p4est3_t * p3);
 sc3_error_t        *p4est3_internal_setup_cut (p4est3_t * p3,
                                                p4est3_gloidx num_uniform,
@@ -106,6 +146,7 @@ sc3_error_t        *p4est3_internal_setup_cut (p4est3_t * p3,
 sc3_error_t        *p4est3_internal_setup_tree (p4est3_t * p3,
                                                 p4est3_gloidx num_uniform);
 sc3_error_t        *p4est3_internal_setup_morton (p4est3_t * p3);
+/** \endcond */
 
 #ifdef __cplusplus
 #if 0
