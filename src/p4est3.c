8271b25c326e40d5a3b2c3edcfd8265d893c7d49
Mikhail Kirilin <kirilin@ins.uni-bonn.de>
2020-12-14 17:02:30 +0100

    Fix: brace in p4est3_setup

--- src/p4est3.c
+++ src/p4est3.c
@@ -310,10 +310,11 @@ p4est3_setup (p4est3_t * p3)
 
     /* create tree and quadrant metadata */
     SC3E (p4est3_internal_setup_tree (p3, num_uniform));
-  }
-  /* create quadrants by the previously specified method,
+
+    /* create quadrants by the previously specified method,
      default is morton, which is presumably slowest */
-  SC3E (p4est3_internal_setup_quadrants (p3));
+    SC3E (p4est3_internal_setup_quadrants (p3));
+  }
 
   /* we are done creating a valid forest */
   p3->setup = 1;
