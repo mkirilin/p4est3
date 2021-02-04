bffe6c0cb09d7126cdedd2ab40852bbb30ff1a50
Mikhail Kirilin <kirilin@ins.uni-bonn.de>
2021-02-04 16:18:47 +0100

    [AVX] Add custom MAXLAVELs

--- src/p4est3_to_p8est3.h
+++ src/p4est3_to_p8est3.h
@@ -29,6 +29,10 @@
 #endif
 #define P4_TO_P8
 
+/* macros in p4est3_quadrant_yx */
+#define P4EST3_YX_MAXLEVEL                   P8EST3_ZYX_MAXLEVEL
+#define P4EST3_YX_QMAXLEVEL                  P8EST3_ZYX_QMAXLEVEL
+
 /* functions in p4est3_p4est */
 #define p4est3_new_p4est                    p4est3_new_p8est
 #define p4est3_connectivity_new_p4est       p4est3_connectivity_new_p8est
