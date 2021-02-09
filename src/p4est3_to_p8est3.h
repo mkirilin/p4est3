603e1de610c46b5850df8732c0b418207e45b774
Mikhail Kirilin <kirilin@ins.uni-bonn.de>
2021-02-09 13:21:26 +0100

    [Mort] P4EST_ -> P4EST3_ maxlevel

--- src/p4est3_to_p8est3.h
+++ src/p4est3_to_p8est3.h
@@ -30,8 +30,8 @@
 #define P4_TO_P8
 
 /* macros in p4est3_quadrant_mort */
-#define P4EST_MORT_MAXLEVEL                P8EST_MORT_MAXLEVEL
-#define P4EST_MORT_QMAXLEVEL               P8EST_MORT_QMAXLEVEL
+#define P4EST3_MORT_MAXLEVEL                P8EST3_MORT_MAXLEVEL
+#define P4EST3_MORT_QMAXLEVEL               P8EST3_MORT_QMAXLEVEL
 
 /* functions in p4est3_p4est */
 #define p4est3_new_p4est                    p4est3_new_p8est
--- src/p8est3_quadrant_mort.h
+++ src/p8est3_quadrant_mort.h
@@ -36,8 +36,8 @@ extern              "C"
 #endif
 
 #define P4EST3_QUADRANT_MORT_LEN(n, l) ((uint64_t) (n) << P4EST_DIM * (P4EST_MAXLEVEL - (l)))
-#define P8EST_MORT_MAXLEVEL 21
-#define P8EST_MORT_QMAXLEVEL 21
+#define P8EST3_MORT_MAXLEVEL 21
+#define P8EST3_MORT_QMAXLEVEL 21
 
 typedef struct p4est3_quadrant_mort
 {
