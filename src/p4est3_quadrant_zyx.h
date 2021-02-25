09fc99bcff3245e2f89bac5318ced032899fa504
Mikhail Kirilin <kirilin@ins.uni-bonn.de>
2021-02-25 19:29:53 +0100

    [AVX] maxlevel: 21 -> 30 in 3d; 31 -> 30 in 2d

--- src/p4est3_quadrant_zyx.h
+++ src/p4est3_quadrant_zyx.h
@@ -41,8 +41,8 @@ extern              "C"
 #endif
 #endif
 
-#define P4EST3_ZYX_MAXLEVEL 21
-#define P4EST3_ZYX_QMAXLEVEL 21
+#define P4EST3_ZYX_MAXLEVEL 30
+#define P4EST3_ZYX_QMAXLEVEL 30
 
 /** Populate a 3D quadrant virtual table with an AVX implementation.
  * We use the level and x, y, z coordinates inside a 4x32 bit hardware type.
