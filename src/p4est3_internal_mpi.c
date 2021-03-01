e1625ab97341d2faa7becce60488c3a4607e059c
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2021-03-01 18:34:44 +0100

    Only disable unkwnon #pragma warning without omp

--- src/p4est3_internal_mpi.c
+++ src/p4est3_internal_mpi.c
@@ -24,6 +24,10 @@
 #include <p4est3_internal.h>
 #include <sc3_omp.h>
 
+#ifndef P4EST_ENABLE_OPENMP
+#pragma GCC diagnostic ignored "-Wunknown-pragmas"
+#endif
+
 sc3_error_t        *
 p4est3_internal_setup_comm (p4est3_t * p3)
 {
