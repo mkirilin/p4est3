c0ca3bb2cef103f4a030ba790a1912c8055abca9
Mikhail Kirilin <kirilin@ins.uni-bonn.de>
2021-05-10 17:23:26 +0200

    p4est3: iterate: add simple volume iterator

--- src/p4est3_iterate.c
+++ src/p4est3_iterate.c
@@ -22,6 +22,7 @@
 */
 
 #include <p4est3_iterate.h>
+#include <p4est3_internal.h>
 
 #ifdef __cplusplus
 extern              "C"
@@ -36,7 +37,31 @@ p4est3_iterate_face (p4est3_t * p3,
                      p4est3_iterate_volume_t * cvolume,
                      p4est3_iterate_face_t * cface, void *user_data)
 {
-  SC3E (p4est3_iterate_codim (p3, 0x03, cvolume, cface, NULL, user_data));
+  SC3E (p4est3_iterate_codim (p3, 0x01, cvolume, cface, NULL, user_data));
+  return NULL;
+}
+
+static sc3_error_t *
+p4est3_internal_iterate_volume_simple (p4est3_t * p3,
+                                       p4est3_iterate_volume_t * cvolume,
+                                       void *user_data)
+{
+  p4est3_topidx       ntree;
+  p4est3_tree_t      *tree;
+  p4est3_iterate_volume_info_t info;
+  size_t              si;
+
+  info.p3 = p3;
+  info.user_data = user_data;
+  for (ntree = p3->fltree; ntree <= p3->lltree; ++ntree) {
+    info.ntree = ntree;
+    SC3E (p4est3_tree_index (p3, ntree, &tree));
+    for (si = 0; si < tree->num_quads; ++si) {
+      info.quadrant = tree->tquads + si * p3->qvt->quadrant_size;
+      info.nquad = si + tree->quad_offset;
+      SC3E (cvolume (&info));
+    }
+  }
   return NULL;
 }
 
@@ -46,6 +71,22 @@ p4est3_iterate_codim (p4est3_t * p3, int codims,
                       p4est3_iterate_face_t * cface,
                       p4est3_iterate_codim_t * ccodim, void *user_data)
 {
+  p4est3_topidx       tree;
+
+  /* This iteration is w/o ghost layer and for volumes only */
+  if (codims < 0 || codims >= P4EST3_ITERATE_LAST) {
+    return NULL;
+  }
+  SC3A_IS (p4est3_is_setup, p3);
+  if (p3->fltree < 0 || cvolume == NULL && cface == NULL && ccodim == NULL) {
+    return NULL;
+  }
+
+  if (codims == P4EST3_ITERATE_VOLUME) {
+    SC3E (p4est3_internal_iterate_volume_simple (p3, cvolume, user_data));
+    return NULL;
+  }
+
   return NULL;
 }
 
