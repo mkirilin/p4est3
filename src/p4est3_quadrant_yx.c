87ad0547be05c9f472ab3c9c4a2521a4aabd0e25
Mikhail Kirilin <kirilin@ins.uni-bonn.de>
2021-09-22 15:38:59 +0200

    avx: Fix coords ordering at tree_face_neighbour

--- src/p4est3_quadrant_yx.c
+++ src/p4est3_quadrant_yx.c
@@ -318,20 +318,23 @@ p4est3_quadrant_zyx_tree_face_neighbor (const __m128i * q,
   int                *my_axis;
   int                *target_axis;
   int                *edge_reverse;
-  int32_t             target_xyzl[4];
-  int32_t             q_as_array[4];
+  int32_t             target_xyzl[3] = { 0, 0, 0 };
+  int32_t             q_coords[3];
   int32_t             level, mh, Rmh;
 
   SC3A_IS (p4est3_quadrant_zyx_is_valid, q);
+  SC3A_CHECK (q != r);
+
   SC3E (sc3_array_index (transform, 0, &my_axis));
   SC3E (sc3_array_index (transform, 3, &target_axis));
-  SC3E (sc3_array_index (transform, 3, &edge_reverse));
+  SC3E (sc3_array_index (transform, 6, &edge_reverse));
 
-  _mm_storeu_si128 ((__m128i *) & q_as_array[0], *q);
+  q_coords[0] = _mm_extract_epi32 (*q, 3);
+  q_coords[1] = _mm_extract_epi32 (*q, 2);
+  q_coords[2] = _mm_extract_epi32 (*q, 1);
+  level = _mm_extract_epi32 (*q, 0);
 
-  SC3A_CHECK (q != r);
 #ifdef P4EST_ENABLE_DEBUG
-
   for (i = 0; i < 3; ++i) {
     SC3A_CHECK (0 <= my_axis[i] && my_axis[i] < P4EST_DIM);
     SC3A_CHECK (0 <= target_axis[i] && target_axis[i] < P4EST_DIM);
@@ -347,14 +350,13 @@ p4est3_quadrant_zyx_tree_face_neighbor (const __m128i * q,
   SC3A_CHECK (target_axis[0] != target_axis[1] &&
               target_axis[1] != target_axis[2]);
   SC3A_CHECK (0 <= edge_reverse[1] && edge_reverse[1] < 2);
-#endif
+#else
   *r = _mm_set_epi32 (INT32_MIN, INT32_MIN, 0, 0);
   SC3A_CHECK (my_axis[1] == 0 && target_axis[1] == 0);
   SC3A_CHECK (edge_reverse[1] == 0);
-#endif
+#endif /* P4_TO_P8 */
+#endif /* P4EST_ENABLE_DEBUG */
 
-  level = q_as_array[0];
-  target_xyzl[3] = level;
   if (level == P4EST3_YX_MAXLEVEL) {
     /* If (P4EST3_YX_MAXLEVEL == 31) Rmh will overflow.
        P4EST3_YX_MAXLEVEL == 30 so far,
@@ -367,34 +369,25 @@ p4est3_quadrant_zyx_tree_face_neighbor (const __m128i * q,
   Rmh = P4EST3_YX_ROOT_LEN - mh;
 
   if (!edge_reverse[0]) {
-    target_xyzl[target_axis[0]] = q_as_array[my_axis[0]];
+    target_xyzl[target_axis[0]] = q_coords[my_axis[0]];
   }
   else {
-    target_xyzl[target_axis[0]] = Rmh - q_as_array[my_axis[0]];
+    target_xyzl[target_axis[0]] = Rmh - q_coords[my_axis[0]];
   }
 #ifdef P4_TO_P8
   if (!edge_reverse[1]) {
-    target_xyzl[target_axis[1]] = q_as_array[my_axis[1]];
+    target_xyzl[target_axis[1]] = q_coords[my_axis[1]];
   }
   else {
-    target_xyzl[target_axis[1]] = Rmh - q_as_array[my_axis[1]];
+    target_xyzl[target_axis[1]] = Rmh - q_coords[my_axis[1]];
   }
-#else
-  target_xyzl[target_axis[1]] = 0;
 #endif
 
-  switch (edge_reverse[2]) {
-  case 1:
+  if (edge_reverse[2] == 1 || edge_reverse[2] == 3) {
     target_xyzl[target_axis[2]] = Rmh;
-    break;
-  case 2:
-    target_xyzl[target_axis[2]] = 0;
-    break;
-  default:
-    /* do nothing to the codes 0 and 3 */
-    break;
   }
-  *r = _mm_loadu_si128 ((__m128i *) & target_xyzl[0]);
+  *r = _mm_set_epi32 (target_xyzl[0], target_xyzl[1], target_xyzl[2], level);
+
   SC3A_IS (p4est3_quadrant_zyx_is_valid, r);
   return NULL;
 }
