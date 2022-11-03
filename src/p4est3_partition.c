c7925f731cb85929740c92a51fe89b26be063f49
Mikhail Kirilin <kirilin@ins.uni-bonn.de>
2022-11-03 18:23:41 +0100

    p3_part: calculate where quads come from

--- src/p4est3_partition.c
+++ src/p4est3_partition.c
@@ -77,6 +77,58 @@ p4est3_translate_quadrant (const p4est3_quadrant_vtable_t * qvt_old,
   return NULL;
 }
 
+/* p4est3::goffsets should be updated before calling this function */
+static sc3_error_t *
+p4est3_procs_recv_from (const p4est3_t * p3, int nodesize, int noderank,
+                        int node_num, int *node_offsets,
+                        p4est3_gloidx *last_goffsets,
+                        p4est3_locidx *num_recv_from,
+                        int *from_begin, int *from_end,
+                        int *num_proc_recv_from)
+{
+  int i, from_proc;
+  p4est3_gloidx my_begin, my_end, lower_bound;
+
+  for (i = 0; i < p3->mpisize; ++i) {
+    last_goffsets[i] = p3->old->goffset[i + 1] - 1;
+  }
+
+  my_begin = p3->goffset[p3->mpirank];
+  my_end = p3->goffset[p3->mpirank + 1] - 1;
+  *num_proc_recv_from = 0;
+
+  if (my_begin > my_end) {
+    /* my_begin == my_end requires a search is legal for find_partition */
+    *from_begin = p3->mpirank;
+    *from_end = p3->mpirank;
+  } else {
+    SC3E (p4est3_find_partition
+          (p3->alloc, p3->mpisize, last_goffsets,
+           my_begin, my_end, from_begin, from_end));
+
+    /* this min and max only because we work within a node so far */
+    *from_begin = SC3_MAX (*from_begin, node_offsets[node_num]);
+    *from_end = SC3_MIN (*from_end, node_offsets[node_num + 1] - 1);
+
+    for (from_proc = *from_begin; from_proc <= *from_end; ++from_proc) {
+      lower_bound = p3->old->goffset[from_proc];
+
+      if ((lower_bound > my_end) || (last_goffsets[from_proc] < my_begin)) {
+        continue;
+      }
+      num_recv_from[from_proc] =
+          SC3_MIN (my_end, last_goffsets[from_proc])
+        - SC3_MAX (my_begin, lower_bound) + 1;
+      SC3A_CHECK (num_recv_from[from_proc] >= 0);
+      if (from_proc == p3->mpirank) {
+        continue;
+      }
+      (*num_proc_recv_from)++;
+    }
+  }
+  return NULL;
+}
+
 sc3_error_t        *
 p4est3_partition (p4est3_t * p3)
 {
@@ -88,6 +140,8 @@ p4est3_partition (p4est3_t * p3)
   int32_t            *coords;
   p4est3_gloidx        prev_quadrant, next_quadrant, qcount_node;
   p4est3_gloidx        new_right_border, new_left_border;
+  p4est3_gloidx *last_goffsets; /**< Offsets of last quadrant in a process */
+  p4est3_locidx *num_recv_from; /**< Numbers of quadrants coming from the i-th process */
   sc3_MPI_Comm_t nodecomm;
 
   /* We suppose to call this function after setting up routine */
@@ -158,7 +212,10 @@ p4est3_partition (p4est3_t * p3)
     p3->quads = p3->nodequads[noderank];
 
     /* Adjust trees partition information of the forest */
-    
+    SC3E (sc3_allocator_malloc
+        (p3->alloc, p3->mpisize * sizeof (p4est3_gloidx), &last_goffsets));
+    SC3E (sc3_allocator_malloc
+        (p3->alloc, p3->mpisize * sizeof (p4est3_locidx), &num_recv_from));
   }
 
 }
