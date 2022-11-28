85176f5c0570a5611eaeaa441939231b71f6b26f
Mikhail Kirilin <kirilin@ins.uni-bonn.de>
2022-11-28 15:51:16 +0100

    p3: part: test: set continious sh. mem.

--- test/test_p3_partition2.c
+++ test/test_p3_partition2.c
@@ -296,6 +296,7 @@ perform_test (p4est3_t * p3, p4est_t * p, setup_t * t,
   SC3E (p4est3_set_source (p3refined, p3ptr));
   SC3E (p4est3_set_partition (p3refined, 1));
   SC3E (p4est3_set_shared (p3refined, 1));
+  SC3E (p4est3_set_contiguous (p3refined, 1));
   SC3E (p4est3_setup (p3refined));
 
   SC3E (p4est3_destroy (&p3ptr));
