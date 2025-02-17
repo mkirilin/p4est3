8b0d461ef44aa230250e9b7e95b2957987102bdc
Mikhail Kirilin <kirilin@ins.uni-bonn.de>
2025-02-17 18:21:23 +0100

    p4est3-ghost: Iterator callback to fill ghosts data (w/o mirrors)

--- src/p4est3_ghost.c
+++ src/p4est3_ghost.c
@@ -23,6 +23,7 @@
 
 #include <p4est3_ghost.h>
 #include <p4est3_internal.h>
+#include <p4est3_search.h>
 
 #ifdef __cplusplus
 extern              "C"
@@ -44,9 +45,14 @@ p4est3_ghost_fill_callback (p4est3_iterate_face_info_t *fi)
   p4est3_ghost_fill_data_t *d = (p4est3_ghost_fill_data_t *) fi->user_data;
   p4est_ghost_t      *ghost = d->ghost;
   p4est3_iterate_face_side_t *fside[2], *gside;
-  size_t               nsides;
+  p4est_quadrant_t    q;
+  size_t              nsides;
+  p4est_topidx_t     *ghosts_in_tree;
+  p4est_gloidx_t     *ghosts_in_proc, proc_owner, global_qid;
+  int                 coords[P4EST_DIM], level;
 
-  ghost = (void *) ghost; /* temporarily to avoid unused variable */
+  /* TODO: Check if the ghost is already in the layer via hash table.
+           If so, skip it. */
 
   SC3E(sc3_array_get_elem_count (fi->sides, &nsides));
   SC3A_CHECK(nsides == 2 || nsides == 1);
@@ -79,12 +85,46 @@ p4est3_ghost_fill_callback (p4est3_iterate_face_info_t *fi)
    * 2. Iterate in the order of increasing ghosts id. Is it even possible?
   */
 
+  /* Convert p3 quad to p2 quad throughout coordinates because of qvt */
+  SC3E (p4est3_quadrant_coordinates (fi->p3->qvt, gside->quadrant, coords));
+  SC3E (p4est3_quadrant_level (fi->p3->qvt, gside->quadrant, &q.level));
+  q.x = coords[0];
+  q.y = coords[1];
+#ifdef P4_TO_P8
+  q.z = coords[2];
+#endif
+  /* Find ghost proc owner */
+  proc_owner = fi->p3->mpirank;
+  global_qid = (p4est3_gloidx) gside->nquad + fi->p3->gtroffset[gside->ntree];
+  /** TODO: We just did it in Iterator to fill callback data.
+   *        Now we do it again here. Think on a way to optimize it. */
+  SC3E (p4est3_search_lower_bound64
+         (global_qid, fi->p3->goffset, fi->p3->mpisize + 1, &proc_owner));
+  if (fi->p3->goffset[proc_owner] > global_qid) {
+    SC3A_CHECK (proc_owner > 0);
+    proc_owner--;
+  }
+
   /** Fill its \c piggy3 field */
+  q.p.piggy3.which_tree = gside->ntree;
+  q.p.piggy3.local_num = global_qid - fi->p3->goffset[proc_owner];
+
+  /* Push back to ghosts array */
+  *(p4est_quadrant_t *) sc_array_push(&(ghost->ghosts)) = q;
 
   /** Contribute to a structure tracking \c tree_offsets */
+
+  ghosts_in_tree =
+    (p4est_locidx_t *) sc_array_index (ghost->tree_offsets, gside->ntree);
+  (*ghosts_in_tree)++;
+
   /** Contribute to a structure tracking \c proc_offsets */
+  ghosts_in_proc =
+    (p4est_locidx_t *) sc_array_index (ghost->proc_offsets, proc_owner);
+  (*ghosts_in_proc)++;
 
   /** Do the same for mirrors */
+  /* Check 1st todo */
 
   return NULL;
 }
@@ -93,7 +133,9 @@ sc3_error_t        *
 p4est3_ghost_fill_p4est (p4est3_t * p3, p4est_ghost_t * ghost)
 {
   /*TODO: Allocate memory for ghosts outside and before this function call */
+  /* Ensure tree_ and proc_offsets are pre-initialized by 0 */
   p4est3_ghost_fill_data_t data, *d = &data;
+  int                i;
   /* ... */
 
   d->ghost = ghost;
@@ -110,8 +152,17 @@ p4est3_ghost_fill_p4est (p4est3_t * p3, p4est_ghost_t * ghost)
 
   SC3E (p4est3_iterate_face (p3, NULL, p4est3_ghost_fill_callback, d));
 
-  /** Fill \c tree_offsets */
-  /** Fill \c proc_offsets */
+  /** Accumulate \c tree_offsets */
+  for (i = 1; i < ghost->num_trees; i++) {
+    (*(p4est_locidx_t *) sc_array_index (ghost->tree_offsets, i)) +=
+      (*(p4est_locidx_t *) sc_array_index (ghost->tree_offsets, i - 1));
+  }
+
+  /** Accumulate \c proc_offsets */
+  for (i = 1; i < ghost->mpisize; i++) {
+    (*(p4est_locidx_t *) sc_array_index (ghost->proc_offsets, i)) +=
+      (*(p4est_locidx_t *) sc_array_index (ghost->proc_offsets, i - 1));
+  }
 
   return NULL;
 }
