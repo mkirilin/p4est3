c89d2677e9cc6f806eaced73d3e8087416bc3d5e
Mikhail Kirilin <kirilin@ins.uni-bonn.de>
2025-03-02 19:06:05 +0100

    p4est3_ghost: test: create p4est2 and p4est3 ghost layers

--- test/test_p3_ghost2.c
+++ test/test_p3_ghost2.c
@@ -1,35 +1,39 @@
 /*
-  This file is part of p4est, version 3.
-  p4est is a C library to manage a collection (a forest) of multiple
-  connected adaptive quadtrees or octrees in parallel.
+This file is part of p4est, version 3.
+p4est is a C library to manage a collection (a forest) of multiple
+connected adaptive quadtrees or octrees in parallel.
 
-  Copyright (C) 2019 individual authors
-  Originally written by Carsten Burstedde, Lucas C. Wilcox, and Tobin Isaac
+Copyright (C) 2019 individual authors
+Originally written by Carsten Burstedde, Lucas C. Wilcox, and Tobin Isaac
 
-  p4est is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
+p4est is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
 
-  p4est is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+p4est is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with p4est; if not, write to the Free Software Foundation, Inc.,
-  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+You should have received a copy of the GNU General Public License
+along with p4est; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 */
 
 #include <p4est_bits.h>
 #include <p4est_ghost.h>
 
 #include <p4est3.h>
+#include <p4est3_internal.h>
 #include <p4est3_ghost.h>
 #include <p4est3_convert_p4est.h>
 
-/*
-static int refine_level = 5;
+#ifndef P4_TO_P8
+static int          refine_level = 5;
+#else
+static int          refine_level = 4;
+#endif
 
 static int
 refine_fn (p4est_t * p4est, p4est_topidx_t which_tree,
@@ -67,72 +71,113 @@ refine_fn (p4est_t * p4est, p4est_topidx_t which_tree,
   }
 
   return 1;
-}*/
+}
 
-int main(int argc, char **argv) {
-    int mpiret;
-    sc_MPI_Comm mpicomm;
-    //p4est_t *p4est;
-    //p4est3_t *p4est3;
-    //p4est_connectivity_t *conn;
-    //p4est_ghost_t *ghost_p4est, *ghost_p4est3;
-    //int num_cycles = 2;
-    //int i;
+static p4est_ghost_t *
+init_ghost_layer (p4est_t *p)
+{
+  const p4est_topidx_t num_trees = p->connectivity->num_trees;
+  p4est_ghost_t     *gl;
 
-    /* initialize MPI */
-    mpiret = sc_MPI_Init(&argc, &argv);
-    SC_CHECK_MPI(mpiret);
-    mpicomm = sc_MPI_COMM_WORLD;
+  gl = P4EST_ALLOC (p4est_ghost_t, 1);
+  sc_array_init (&gl->ghosts, sizeof (p4est_quadrant_t));
+  gl->tree_offsets = P4EST_ALLOC (p4est_locidx_t, num_trees + 1);
+  gl->proc_offsets = P4EST_ALLOC (p4est_locidx_t, p->mpisize + 1);
 
-    sc_init(mpicomm, 1, 1, NULL, SC_LP_DEFAULT);
-    p4est_init(NULL, SC_LP_DEFAULT);
+  sc_array_init (&gl->mirrors, sizeof (p4est_quadrant_t));
+  gl->mirror_tree_offsets = P4EST_ALLOC (p4est_locidx_t, num_trees + 1);
+  gl->mirror_proc_mirrors = NULL;
+  gl->mirror_proc_offsets = P4EST_ALLOC (p4est_locidx_t, p->mpisize + 1);
 
-    //conn = p4est_connectivity_new_unitsquare();
-    //p4est = p4est_new(mpicomm, conn, 0, NULL, NULL);
+  return gl;
+}
 
-    /* refine to make the number of elements interesting */
-    //p4est_refine(p4est, 1, refine_fn, NULL);
+static sc3_error_t *
+make_allocator (sc3_allocator_t * oa, sc3_allocator_t ** alloc)
+{
+  SC3A_IS (sc3_allocator_is_setup, oa);
+  SC3E (sc3_allocator_new (oa, alloc));
+  SC3E (sc3_allocator_setup (*alloc));
+  return NULL;
+}
 
-    /* balance the forest */
-    //p4est_balance(p4est, P4EST_CONNECT_FULL, NULL);
 
-    /* do a uniform partition */
-    //p4est_partition(p4est, 0, NULL);
+int main(int argc, char **argv)
+{
+  int mpiret;
+  sc_MPI_Comm mpicomm;
+  p4est_t *p4est;
+  p4est3_t *p4est3;
+  p4est_connectivity_t *conn;
+  p4est_ghost_t *ghost_p4est, *ghost_p4est3;
+  sc3_allocator_t    *alloc, *mainalloc;
+  sc3_error_t        *e = NULL;
+  
+  /* initialize MPI */
+  mpiret = sc_MPI_Init(&argc, &argv);
+  SC_CHECK_MPI(mpiret);
+  mpicomm = sc_MPI_COMM_WORLD;
 
-    /* create the ghost layer for p4est */
-    //ghost_p4est = p4est_ghost_new(p4est, P4EST_CONNECT_FULL);
+  sc_init(mpicomm, 1, 1, NULL, SC_LP_DEFAULT);
+  p4est_init(NULL, SC_LP_DEFAULT);
 
-    /* convert p4est to p4est3 */
-    //p4est3_new(mpicomm, &p4est3);
-    //p4est3_convert_p4est(p4est, p4est3);
+  /*--------------------------------------------------------------*/
+  /************************** P4EST 2 *****************************/
+  /*--------------------------------------------------------------*/
 
-    /* create the ghost layer for p4est3 */
-    //ghost_p4est3 = p4est_ghost_new(p4est, P4EST_CONNECT_FULL);
-    //p4est3_ghost_fill_p4est(p4est3, ghost_p4est3);
+#ifndef P4_TO_P8
+  conn = p4est_connectivity_new_moebius ();
+#else
+  conn = p8est_connectivity_new_rotcubes ();
+#endif
 
-    /* compare ghost layers */
-    //SC_CHECK_ABORT(ghost_p4est->ghosts.elem_count == ghost_p4est3->ghosts.elem_count,
-    //               "Ghost count mismatch between p4est and p4est3");
+  p4est = p4est_new(mpicomm, conn, 0, NULL, NULL);
 
-    //for (i = 0; i < ghost_p4est->ghosts.elem_count; ++i) {
-    //    p4est_quadrant_t *q_p4est = p4est_quadrant_array_index(&ghost_p4est->ghosts, i);
-    //    p4est_quadrant_t *q_p4est3 = p4est_quadrant_array_index(&ghost_p4est3->ghosts, i);
-    //    SC_CHECK_ABORT(p4est_quadrant_is_equal(q_p4est, q_p4est3),
-    //                   "Ghost quadrant mismatch between p4est and p4est3");
-    //}
+  /* refine to make the number of elements interesting */
+  p4est_refine(p4est, 1, refine_fn, NULL);
+  
+  /* balance the forest */
+  p4est_balance(p4est, P4EST_CONNECT_FULL, NULL);
 
-    /* clean up */
-    //p4est_ghost_destroy(ghost_p4est);
-    //p4est_ghost_destroy(ghost_p4est3);
-    //p4est_destroy(p4est);
-    //p4est3_destroy(&p4est3);
-    //p4est_connectivity_destroy(conn);
+  /* do a uniform partition */
+  p4est_partition(p4est, 0, NULL);
 
-    /* exit */
-    sc_finalize();
+  /* create the ghost layer for p4est */
+  ghost_p4est = p4est_ghost_new(p4est, P4EST_CONNECT_FACE);
 
-    mpiret = sc_MPI_Finalize();
-    SC_CHECK_MPI(mpiret);
+  /*--------------------------------------------------------------*/
+  /************************** P4EST 3 *****************************/
+  /*--------------------------------------------------------------*/
 
-    return 0;
+  mainalloc = sc3_allocator_nothread ();
+  SC3E_NULL_SET (e, make_allocator (mainalloc, &alloc));
+  SC3E_NULL_SET (e, p4est3_new (alloc, &p4est3));
+  SC3E_NULL_SET (e, p4est3_set_shared (p4est3, 1));
+  SC3E_NULL_SET (e, p4est3_set_contiguous (p4est3, 1));
+
+  SC3E_NULL_SET (e, p4est3_convert_p4est (p4est, p4est3));
+
+  ghost_p4est3 = init_ghost_layer (p4est);
+
+  SC3E_NULL_SET (e, p4est3_ghost_fill_p4est (p4est3, ghost_p4est3));
+
+  /* tenporatily, to avoid unused variable error */
+  ghost_p4est = (void *) ghost_p4est;
+  ghost_p4est3 = (void *) ghost_p4est3;
+
+  /* clean up */
+  //p4est_ghost_destroy(ghost_p4est);
+  //p4est_ghost_destroy(ghost_p4est3);
+  //p4est_destroy(p4est);
+  //p4est3_destroy(&p4est3);
+  //p4est_connectivity_destroy(conn);
+
+  /* exit */
+  SC3E_NULL_REQ (e, !sc_finalize_noabort ());
+
+  mpiret = sc_MPI_Finalize();
+  SC_CHECK_MPI(mpiret);
+
+  SC3X (e);
+  return 0;
 }
