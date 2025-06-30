3b823f9ced593eb2f2ff6f0edb7e2c8aae53ce7e
Mikhail Kirilin <kirilin@ins.uni-bonn.de>
2025-06-30 11:45:42 +0200

    p4est3: turn mpi duplicate back on

--- src/p4est3_internal_mpi.c
+++ src/p4est3_internal_mpi.c
@@ -888,7 +888,7 @@ p4est3_internal_setup_from_source (p4est3_t *p3)
   }
   /* variables set before p4est3_setup */
   /* this call also sets p4est3_t::commdup */
-  SC3E (p4est3_set_comm (p3, old->mpicomm, 0));
+  SC3E (p4est3_set_comm (p3, old->mpicomm, 1));
 
   /* this call also sets p4est3_t::num_trees */
   SC3E (p4est3_set_connectivity (p3, old->conn));
